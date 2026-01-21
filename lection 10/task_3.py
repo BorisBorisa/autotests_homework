04ea7c00aa644e69f823df44639405894e218778
an.borisov <an.borisov1@tensor.ru>
2026-01-21 15:41:22 +0300

    fix task_3

--- lection 10/task_3.py
+++ lection 10/task_3.py
@@ -13,26 +13,14 @@ def all_division(*arg1):
 
 class TestTask3:
     @pytest.mark.parametrize(
-        "a,b,result",
+        "args,result",
         [
-            (8, 2, 4),
-            pytest.param(9, 3, 3, marks=pytest.mark.smoke),
-            pytest.param(25, 5, 5, marks=pytest.mark.skip('broken'))
+            ((8, 2), 4),
+            ((100, 50, 2), 1),
+            ((-16, 4, 2), -2),
+            pytest.param((9, 3), 3, marks=pytest.mark.smoke),
+            pytest.param((100, 0), 0, marks=pytest.mark.skip('broken'))
         ]
     )
-    def test_1(self, a, b, result):
-        assert all_division(a, b) == result
-
-    @pytest.mark.smoke
-    def test_2(self):
-        assert all_division(8, 2, 2) == 2
-
-    @pytest.mark.smoke
-    def test_3(self):
-        assert all_division(-16, 4, 2) == -2
-
-    def test_4(self):
-        assert all_division(100, 50, 2, 1) == 1
-
-    def test_5(self):
-        assert all_division(100, 0) == 0
+    def test(self, args, result):
+        assert all_division(*args) == result
